f52f9065734b967393d342357c65bd0b0a54c06c
Sincal <sincaljulio200@gmail.com>
2023-07-28 22:54:20 -0600

    Creación de la función dividir y multiplicar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,11 +15,24 @@ public class Main {
         // Realizar la suma y la resta
         int suma = numero1 + numero2;
         int resta = numero1 - numero2;
+        double multiplicacion = numero1 * numero2;
+
+        // Verificar que el segundo número no sea cero para evitar división por cero
+        double division = 0;
+        if (numero2 != 0) {
+            division = numero1 / numero2;
+        }
 
         // Mostrar los resultados en pantalla
         System.out.println("La suma de los numeros es: " + suma);
         System.out.println("La resta de los numeros es: " + resta);
+        System.out.println("La multiplicacion de los numeros es: " + multiplicacion);
 
+        if (numero2 != 0) {
+            System.out.println("La division de los numeros es: " + division);
+        } else {
+            System.out.println("No se puede realizar la division, el segundo numero es cero.");
+        }
         // Cerrar el objeto Scanner
         sc.close();
     }
